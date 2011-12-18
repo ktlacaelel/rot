aa815ef238e68d34ceb66d51fca5e305b28594ee
kazuyoshi tlacaelel <kazu.dev@gmail.com>
2011-12-17 19:33:54 -0600

    Multibyte & Multichar support.

--- lib/rot.rb
+++ lib/rot.rb
@@ -1,19 +1,19 @@
 class Rot
 
-  attr_accessor :mappings
-  attr_accessor :unknown_mapping
+  attr_accessor :mappings, :unknown_mapping, :tokenizer
 
-  def initialize mappings, unknown_mapping
+  def initialize mappings, unknown_mapping, tokenizer
     @mappings        = mappings
     @unknown_mapping = unknown_mapping
+    @tokenizer       = tokenizer
   end
 
   def decrypt word
-    word.scan(/./).map { |char| @mappings[char] || @unknown_mapping } * ''
+    word.scan(@tokenizer).map { |char| @mappings[char] || @unknown_mapping } * ''
   end
 
   def encrypt word
-    word.scan(/./).map { |char| @mappings.invert[char] || @unknown_mapping } * ''
+    word.scan(@tokenizer).map { |char| @mappings.invert[char] || @unknown_mapping } * ''
   end
 
 end
--- rot.gemspec
+++ rot.gemspec
@@ -5,11 +5,11 @@
 
 Gem::Specification.new do |s|
   s.name = %q{rot}
-  s.version = "0.0.0"
+  s.version = "0.0.1"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["kazuyoshi tlacaelel"]
-  s.date = %q{2011-12-16}
+  s.date = %q{2011-12-17}
   s.description = %q{Simple tool to help translating from one alphabet to another.}
   s.email = %q{kazu.dev@gmail.com}
   s.extra_rdoc_files = [
