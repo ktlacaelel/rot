733e958c777ed7ac45b0bd6c4de5bc5c532348b2
kazuyoshi tlacaelel <kazu.dev@gmail.com>
2011-12-22 19:18:51 -0600

    Encyption and Decryption parsing.

--- lib/rot.rb
+++ lib/rot.rb
@@ -1,19 +1,20 @@
 class Rot
 
-  attr_accessor :mappings, :unknown_mapping, :tokenizer
+  attr_accessor :mappings, :unknown_mapping, :encryption_tokenizer, :decryption_tokenizer
 
-  def initialize mappings, unknown_mapping, tokenizer
-    @mappings        = mappings
-    @unknown_mapping = unknown_mapping
-    @tokenizer       = tokenizer
+  def initialize mappings, unknown_mapping, encryption_tokenizer, decryption_tokenizer
+    @mappings                   = mappings
+    @unknown_mapping            = unknown_mapping
+    @encryption_tokenizer       = encryption_tokenizer
+    @decryption_tokenizer       = decryption_tokenizer
   end
 
   def decrypt word
-    word.scan(@tokenizer).map { |char| @mappings[char] || @unknown_mapping } * ''
+    word.scan(@decryption_tokenizer).map { |char| @mappings[char] || @unknown_mapping } * ''
   end
 
   def encrypt word
-    word.scan(@tokenizer).map { |char| @mappings.invert[char] || @unknown_mapping } * ''
+    word.scan(@encryption_tokenizer).map { |char| @mappings.invert[char] || @unknown_mapping } * ''
   end
 
 end
--- rot.gemspec
+++ rot.gemspec
@@ -5,11 +5,11 @@
 
 Gem::Specification.new do |s|
   s.name = %q{rot}
-  s.version = "0.0.1"
+  s.version = "0.0.2"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["kazuyoshi tlacaelel"]
-  s.date = %q{2011-12-17}
+  s.date = %q{2011-12-21}
   s.description = %q{Simple tool to help translating from one alphabet to another.}
   s.email = %q{kazu.dev@gmail.com}
   s.extra_rdoc_files = [
